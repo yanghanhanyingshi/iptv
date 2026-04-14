e6369f0842760ce02a2878e94de506b700888428
yanghanhanyingshi <159672867+yanghanhanyingshi@users.noreply.github.com>
2026-04-14 19:53:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ def main():
     CURRENT_BJ_TIME = get_beijing_time()
     print(f"脚本运行北京时间：{CURRENT_BJ_TIME}")
 
-    time_url = "https://d.kstore.dev/download/7547/20260401003530.mp4"
+    time_url = "http://niuniuyun.ccwu.cc/T/Videos/20260415.mp4"
 
     all_raw = []
     for src in SOURCES:
