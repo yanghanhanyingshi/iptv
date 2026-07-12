b346e025ba95399468fff2abbb929e5f378e3a29
yanghanhanyingshi <159672867+yanghanhanyingshi@users.noreply.github.com>
2026-07-13 00:24:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ SOURCES = [
     "https://raw.githubusercontent.com/fafa002/yf2025/refs/heads/main/yiyifafa.txt",
     "https://dianshi.xinlingmiyu520.workers.dev/abc123",
     "https://live.445569.xyz/live.m3u",
+    "http://wangziduoqing.com/yuan/zb.txt",
     "https://gh-proxy.org/https://github.com/yanghanhanyingshi/JYYS/blob/main/live_sources.m3u",
     "https://restless-rice-b2a2.ganpig.workers.dev/cfdownload/https://tv.88888888888888888888888888888888888.ccwu.cc/live.m3u"
     "https://g.blfrp.cn/https://raw.githubusercontent.com/cyh92/live/refs/heads/main/source/migu.m3u"
